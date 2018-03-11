a4ce9cf1fa66f628fcbeacc863a64e3e836780ab
kbagot <42.fr>
2018-03-11 20:41:42 +0100

    Home content

--- objects/loadhome.js
+++ objects/loadhome.js
@@ -4,12 +4,17 @@ class Loadhome {
             let [maxspop] = await db.query("SELECT MAX(spop) AS maxspop FROM users");
             let [req] = await db.query("SELECT * FROM users INNER JOIN location ON location.logid = users.id WHERE users.id = ?", [sess.data.id]);
 
+            let sort = '';
+            if (opt === 'star')
+                sort = "ORDER BY spop DESC LIMIT 4";
+            else
+                sort = "WHERE users.id IN (?)";
             let sql = "SELECT users.login, users.first, users.age, users.sexe, users.bio, users.orientation, " +
                 "users.tags, ROUND(users.spop / ?) AS spop, users.date, location.city, location.country, location.zipcode, img.imgid, users.id, likes.user1, likes.user2, likes.matcha, " +
                 "(st_distance_sphere(POINT(lon, lat), POINT(?, ?)) / 1000) AS distance " +
                 " from users INNER JOIN location ON location.logid = users.id LEFT JOIN img ON img.userid = users.id AND (img.profil = 1) " +
                 "LEFT JOIN likes ON ((likes.user2 = " + req[0].id + " AND likes.user1 = users.id) OR (likes.user1 = " + req[0].id + " AND likes.user2 = users.id))" +
-                "WHERE users.id IN (?)";
+                sort;
             // if (opt.task === 'visits') {
             let inserts = [maxspop[0].maxspop / 10, req[0].lon, req[0].lat, opt];
             sql = db.format(sql, inserts);
